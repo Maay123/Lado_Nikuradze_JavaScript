b5e365ea049e6c6fabbf993b5bb27da59279a38f
Maay123 <vladimer.nikuradze@gau.edu.ge>
2020-10-23 15:48:07 +0400

    davaleba

---
+++ Week_4_Davaleba/java.js
@@ -0,0 +1,19 @@
+function currentTime(){
+    result = ``
+    dateTime = new Date();
+    hour = dateTime.getHours()
+    minute = dateTime.getMinutes()
+    second = dateTime.getSeconds()
+
+    if(hour < 10) hour = `0${hour}`;
+    if(minute < 10) minute = `0${minute}`
+    if(second < 10) second = `0${second}`
+
+    result += `${hour}:${minute}:${second}`
+
+    return result
+}
+
+function updateClock(){
+    document.getElementById("clock1").innerHTML = currentTime();
+}
